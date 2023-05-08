4f9427c432b3119b28e8158eee9ce56e58c29989
Sean Henderson <91705235+sean-henderson@users.noreply.github.com>
2023-05-08 14:43:41 -0400

    Deleting outdated demo

--- Game_demo/src/data/img/Interior-01.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="Interior-01" tilewidth="16" tileheight="16" tilecount="625" columns="25">
- <image source="Interior-01.png" width="400" height="400"/>
-</tileset>
--- Game_demo/src/data/img/Tree-Assets.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="Tree-Assets" tilewidth="16" tileheight="16" tilecount="525" columns="21">
- <image source="Tree-Assets.png" width="336" height="400"/>
-</tileset>
