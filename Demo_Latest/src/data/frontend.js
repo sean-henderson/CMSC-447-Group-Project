c79c8cb0582418639c20b5af3e3ac9d792689ffc
Alex Lloyd <alloyd2@umbc.edu>
2023-05-10 22:36:42 -0400

    Fix leaderboard menu link

--- Demo_Latest/src/data/frontend.js
+++ Demo_Latest/src/data/frontend.js
@@ -42,6 +42,6 @@ export const loadGame = () => {
 }
 
 export const getLeaderboard = () => {
-    window.open('http://127.0.0.1:5000/', '_blank');
+    window.open('http://127.0.0.1:8080/leaderboard.html', '_blank');
     return 0;
 }
