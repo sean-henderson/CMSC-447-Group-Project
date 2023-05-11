464e375fb7ba1b2db398b2d581363fbef01dec25
Sean Henderson <91705235+sean-henderson@users.noreply.github.com>
2023-05-10 21:09:28 -0400

    Update coin.js

--- Demo_Latest/src/js/renderables/coin.js
+++ Demo_Latest/src/js/renderables/coin.js
@@ -1,5 +1,5 @@
-//seans file so ask if confused
 import { Collectable, game, collision, audio } from 'melonjs';
+import data from './../../data/data.js';
 
 class CoinEntity extends Collectable {
     // constructor
@@ -15,7 +15,7 @@ class CoinEntity extends Collectable {
     // handles the removal of a coin when player collides with it
     onCollision() {
         // do something when collected
-
+        data.coins += 1;
         // make sure it cannot be collected "again"
         this.body.setCollisionMask(collision.types.NO_OBJECT);
 
